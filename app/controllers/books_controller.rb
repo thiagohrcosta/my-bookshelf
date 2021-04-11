145b5b727c1c632d109aa022253756d2d096a9c6
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-11 14:21:16 -0300

    Create Index, Show and New actions on controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,5 +3,32 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :resume, :year, :pages, :cover)
+  end
+
 
 end
