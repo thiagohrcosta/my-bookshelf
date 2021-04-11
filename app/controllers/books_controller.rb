f58d73a53f72c5d9e1145efcd6fc04470e322e5c
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-11 14:54:38 -0300

    Action edit and updated created

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,9 +16,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
