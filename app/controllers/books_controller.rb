ce2a5f0ebd82460da6f6633393b78fd44459c733
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-11 19:49:05 -0300

    Fix typo error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,10 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
 
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
 
-  destroy "books/:id", to: "books#destroy", as: :destroy_book
+  delete "books/:id", to: "books#destroy", as: :destroy_book
 end
